b46ab4c3ec5b925edd867917e879fa3b527e97d5
Nolan Rummel <nolan.rummel22@gmail.com>
2023-11-29 15:53:55 -0500

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import '../App.css';
+import { ReactComponent as BikeIcon } from '../icons/bike-icon.svg'
+
+export default function App() {
+  return (
+    <div>
+      <h1 className="text-3xl underline">
+        Hello world!
+      </h1>
+      {/* <BikeIcon /> */}
+    </div>
+  )
+}
